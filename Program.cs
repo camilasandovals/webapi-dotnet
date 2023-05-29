29b0fda418ac690ac75cc02e29d0aa2ac2617e47
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-05-29 16:12:49 -0400

    created controller

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IHelloWorldService, HelloWorldService>(); //injector configuration of the dependency
+// builder.Services.AddScoped<IHelloWorldService, HelloWorldService>(); //injector configuration of the dependency
+builder.Services.AddScoped<IHelloWorldService>(p => new HelloWorldService());
 
 var app = builder.Build();
 
